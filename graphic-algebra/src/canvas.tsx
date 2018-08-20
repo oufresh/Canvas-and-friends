f32b0e91192bc9f8bf84236361791266476f4a3e
oufresh <sergio.gennari@gmail.com>
2018-08-20 16:21:23 +0200

    Spostamento di 3 vertici

--- graphic-algebra/src/canvas.tsx
+++ graphic-algebra/src/canvas.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as matrix from './matrix';
+import * as glMatrix from 'gl-matrix';
 
 export type CanvasProps = {
     width: number;
@@ -21,17 +22,15 @@ class Canvas extends React.Component
     {
         this.cContext = this.cRef.current.getContext('2d');
 
-        matrix.Test((vec: Array<number>)=>{
+        matrix.TestTranslate((av) => {
             this.cContext.clearRect(0, 0, 640, 480);
             this.cContext.fillStyle = "rgb(200,0,0)";  
             this.cContext.beginPath();
-            this.cContext.moveTo(vec[0], vec[1]);
-            this.cContext.lineTo(100, 75);
-            this.cContext.lineTo(100, 25);
+            this.cContext.moveTo(av[0][0], av[0][1]);
+            this.cContext.lineTo(av[1][0], av[1][1]);
+            this.cContext.lineTo(av[2][0], av[2][1]);
             this.cContext.fill();
-            //
-            //this.cContext.fillRect(vec[0], vec[1], 55, 50);
-        });
+        })
     }
 
     render()
--- graphic-algebra/src/matrix.ts
+++ graphic-algebra/src/matrix.ts
@@ -6,7 +6,9 @@
 
 import * as glMatrix from 'gl-matrix';
 
-export function Test(cb: Function): void
+declare type testCb = (v: glMatrix.vec2) => void;
+
+export function Test(cb: testCb): void
 {
     //creo un vettore che rappresenta un vertice
     const aVec = glMatrix.vec2.create();
@@ -44,4 +46,53 @@ export function Test(cb: Function): void
         i++;
     }, 2000);
     
+}
+
+declare type testTranslateCb = (vs: Array<glMatrix.vec2>) => void;
+
+export function TestTranslate(cb: testTranslateCb)
+{
+    const va = glMatrix.vec2.create();
+    const vb = glMatrix.vec2.create();
+    const vc = glMatrix.vec2.create();
+
+    va[0] = 10;
+    va[1] = 20;
+    vb[0] = 100;
+    vb[1] = 75;
+    vc[0] = 100;
+    vc[1] = 25;
+
+    //parto dalla matrice identità
+    const resMat = glMatrix.mat2d.create();
+
+    //per un certo numero di cicli, come in un movimento, calcolo una nuova matrice di trasformazione
+    let i = 0;
+    const deltaX = 5;
+    const tVec = glMatrix.vec2.create();
+    tVec[0] = deltaX;
+
+    const tId = setInterval(()=>{
+        const tMat = glMatrix.mat2d.clone(resMat);
+
+        glMatrix.mat2d.translate(resMat, tMat, tVec);
+
+        //console.log(aMat.toString());
+
+
+        //trasformo il vettore moltiplicando per la matrice per trasformare il vettore -> in questo caso solo traslazione
+        const outA = glMatrix.vec2.create();
+        const outB = glMatrix.vec2.create();
+        const outC = glMatrix.vec2.create();
+        glMatrix.vec2.transformMat2d(outA, va, resMat);
+        glMatrix.vec2.transformMat2d(outB, vb, resMat);
+        glMatrix.vec2.transformMat2d(outC, vc, resMat);
+        //console.log('vettore trasformato: ' + out.toString());
+        cb([outA, outB, outC]);
+
+        if (i >= 10)
+            clearInterval(tId);
+
+        i++;
+    }, 2000);
 }
