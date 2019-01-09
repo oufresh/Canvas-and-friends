339d2ef340e54f7ca9e83667ea02d057366706eb
oufresh <sergio.gennari@gmail.com>
2019-01-09 23:36:48 +0100

    deleted unused

--- graphic-algebra-tmp/src/collisions/pointInCircle.ts
+++
@@ -1,9 +0,0 @@
-import { calcDistance } from './collisionCals';
-
-export function pointInCircle(px: number, py: number, cx: number, cy: number, r: number): boolean {
-    const distance = calcDistance(cx, cy, px, py);
-    if (distance <= r) {
-        return true;
-    }
-    return false;
-}
--- graphic-algebra-tmp/src/shapes/base.ts
+++
@@ -1,3 +0,0 @@
-export interface Shape { 
-    id: string;
-}
